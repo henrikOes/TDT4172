1bd76e033d20a5c4503613aece291fcab3f3d309
henrikOes <henrik.ostvik@gmail.com>
2025-08-29 15:29:03 +0200

    first push

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -2,11 +2,13 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self):
-        # NOTE: Feel free to add any hyperparameters 
-        # (with defaults) as you see fit
+    def __init__(self): 
+        self.weights, self.bias = None, None
+        self.epochs = 1000
+        self.learning_rate = 0.1
+        self.losses, self.train_accuracies = [], []
         pass
-        
+    
     def fit(self, X, y):
         """
         Estimates parameters for the classifier
@@ -16,9 +18,11 @@ class LinearRegression():
                 m rows (#samples) and n columns (#features)
             y (array<m>): a vector of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The fit method is not implemented yet.")
-    
+        self.weights = np.zeros(x.shape[1])
+        self.bias = 0
+        
+        for _ in self.epochs
+        
     def predict(self, X):
         """
         Generates predictions
